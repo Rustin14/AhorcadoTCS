9f25c21c8a801c920c2dc2bec86df5eae1980a22
Gabriel Flores <gabofl14@gmail.com>
2022-05-30 17:20:21 -0500

    Servicios de Iniciar Sesión y Registrar

---
+++ ServiciosAhorcado/AhorcadoSVC.svc.cs
@@ -0,0 +1,23 @@
+﻿using ServiciosAhorcado.Modelo.DAO;
+using ServiciosAhorcado.Modelo.Poco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiciosAhorcado
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "AhorcadoSVC" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione AhorcadoSVC.svc o AhorcadoSVC.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class AhorcadoSVC : IAhorcadoSVC
+    {
+        public RespuestaLogin LogIn(String username, String password)
+        {
+            RespuestaLogin respuesta = new RespuestaLogin();
+            respuesta = UsuarioDAO.iniciarSesion(username, password);
+            return respuesta;
+        }
+    }
+}
---
+++ ServiciosAhorcado/IAhorcadoSVC.cs
@@ -0,0 +1,18 @@
+﻿using ServiciosAhorcado.Modelo.Poco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiciosAhorcado
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IAhorcadoSVC" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IAhorcadoSVC
+    {
+        [OperationContract]
+        RespuestaLogin LogIn(String username, String password);
+    }
+}
