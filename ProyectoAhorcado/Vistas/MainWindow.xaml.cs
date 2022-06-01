b1d883551a570227fa2df580229828ab1279d9df
Gabriel Flores <gabofl14@gmail.com>
2022-06-01 13:00:07 -0500

    Iniciación de CU Jugar Partida

---
+++ ProyectoAhorcado/Vistas/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ServiciosAhorcado.Modelo.DAO;
+using ServiciosAhorcado.Modelo.Poco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoAhorcado
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ServiciosAhorcado.AhorcadoSVC serviciosAhorcado = new ServiciosAhorcado.AhorcadoSVC();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UsuarioDAO usuarioDAO = new UsuarioDAO();
+            RespuestaLogin respuestaLogin = new RespuestaLogin();
+            String email = emailtb.Text;
+            String password = passwordtb.Text;
+            respuestaLogin = serviciosAhorcado.LogIn(email, password);
+            if (respuestaLogin.UsuarioCorrecto)
+            {
+                
+            } else
+            {
+
+            }
+
+        }
+
+    }
+}
---
+++ ServiciosAhorcado/Modelo/GameUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiciosAhorcado.Modelo
+{
+    public class GameUtil
+    {
+        public String letraSeleccionada { get; set; }
+        public String palabraSeleccionada { set; get; }
+        int partesAhorcado = 0;
+
+        public void IniciarPartida(string palabra)
+        {
+            palabraSeleccionada = palabra;
+            Boolean juegoFinalizado = false;
+            while (partesAhorcado < 6 || juegoFinalizado == false)
+            {
+               
+            }
+        }
+
+        public Char[] verificarLetra(Char[] palabra)
+        {
+            if (letraSeleccionada.IndexOf(palabraSeleccionada, StringComparison.CurrentCultureIgnoreCase) > 0)
+            {
+
+            }
+            else if (letraSeleccionada.IndexOf(palabraSeleccionada, StringComparison.CurrentCultureIgnoreCase) != -1)
+            {
+                partesAhorcado++;
+            }
+        }
+
+    }
+
+    
+}
